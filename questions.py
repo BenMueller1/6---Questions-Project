632ffd33052ab6dcfe15ad3f549b7cac662ec8ae
Ben Mueller <benjm2002@gmail.com>
2022-05-13 00:30:14 -0500

    some calculations must be wrong

--- questions.py
+++ questions.py
@@ -1,7 +1,9 @@
 import nltk
+#  nltk.download('stopwords')   only have to run this once
 import sys
 import os
 import string
+import math
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -53,7 +55,7 @@ def load_files(directory):
     dict = {}
     for filename in os.listdir(directory):
         filepath = os.path.join(directory, filename)
-        with open(filepath) as file:
+        with open(filepath, encoding="utf8") as file:
             dict[filename] = file.read()
     return dict
 
@@ -70,10 +72,30 @@ def tokenize(document):
     finalwords = []
     for i in range(len(words)):
         words[i] = words[i].lower()
-        if any(c.isalpha() for c in words[i]):
-            finalwords.append(words[i])
+        if words[i] not in string.punctuation and words[i] not in nltk.corpus.stopwords.words("english"):
+            if any(c.isalpha() for c in words[i]):
+                finalwords.append(words[i])
     return finalwords
 
+
+def get_all_unique_words(documents):
+    # just need to compute idf for words we are seeing for first time
+    words = set()
+    for document in documents.values():
+        for word in document:
+            words.add(word)
+    return list(words)
+
+def compute_idf(word, documents):
+    # idf is defined as log[(num docs)/(num docs containing word)]
+    num_documents = len(list(documents.values()))
+    num_docs_containing_word = 0
+    for document in documents:
+        if word in document:
+            num_docs_containing_word += 1
+    if num_docs_containing_word == 0:
+        return 0   # IS THIS CORRECT? IS THE IDF ZERO WHEN ITS NOT PRESENT IN ANY DOC?
+    return math.log2(num_documents/num_docs_containing_word)
 
 def compute_idfs(documents):
     """
@@ -83,10 +105,20 @@ def compute_idfs(documents):
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
     """
-    # just need to compute idf for words we are seeing for first time
-    words_seen = set()
-    raise NotImplementedError
+    words_to_idfs = {}
+    unique_words = get_all_unique_words(documents)
+    for word in unique_words:
+        words_to_idfs[word] = compute_idf(word, documents)
+    return words_to_idfs
 
+
+def find_top_n_scores(n, files_to_scores):
+    top_n_filenames = []
+    for _ in range(n):
+        top = max(files_to_scores, key=files_to_scores.get)  # gets key with maximum value
+        del files_to_scores[top]
+        top_n_filenames.append(top)
+    return top_n_filenames
 
 def top_files(query, files, idfs, n):
     """
@@ -95,7 +127,23 @@ def top_files(query, files, idfs, n):
     to their IDF values), return a list of the filenames of the the `n` top
     files that match the query, ranked according to tf-idf.
     """
-    raise NotImplementedError
+    files_to_scores = {} # maps each doc to a score representing how relevant it is to query
+    for title, document in files.items():
+        score = 0
+        for word in query:
+            score += document.count(word) * idfs[word]  # will add nothing if the word isnt present
+        files_to_scores[title] = score
+
+    # we must find and remove (and add to return list) the max from files_to_scores n times
+    return find_top_n_scores(n, files_to_scores)
+
+
+def get_query_term_density(query, sentence):
+    query_words = 0
+    for word in sentence:
+        if word in query:
+            query_words += 1
+    return (query_words/len(sentence))
 
 
 def top_sentences(query, sentences, idfs, n):
@@ -106,8 +154,25 @@ def top_sentences(query, sentences, idfs, n):
     the query, ranked according to idf. If there are ties, preference should
     be given to sentences that have a higher query term density.
     """
-    raise NotImplementedError
+    # Sentences should be ranked according to “matching word measure”: namely, 
+    # the sum of IDF values for any word in the query that also appears in the sentence.
+    sentence_and_score_and_QTD = []
+    for sentence, words in sentences.items():
+        score = 0
+        query_term_density = get_query_term_density(query, words)
+        for word in words:
+            if word in query:
+                score += idfs[word]
+        sentence_and_score_and_QTD.append([sentence, score, query_term_density])
 
+    # sorts by the scores then by the QTD if there is a tie
+    sentence_and_score_and_QTD.sort(key = lambda triple: (triple[1], triple[2]), reverse=True)  
+    breakpoint()
+    # return the top n sentences based on this sorting
+    top_n_sentences = []
+    for i in range(n):
+        top_n_sentences.append(sentence_and_score_and_QTD[i][0])
+    return top_n_sentences
 
 if __name__ == "__main__":
     main()
